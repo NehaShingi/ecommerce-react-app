3453c61fc7d17c180b00443182d2a6d418abb93f
NehaShingi <shingineha836@gmail.com>
2026-05-24 17:54:15 +0530

    Complete authentication page layouts

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 const Auth = () => {
+  const [mode,setMode]=useState(""); //for changing UI based on login or signup mode
   return (
-    <div>
-      Auth Page
+    <div className="page">
+      <div className="container">
+        <div className="auth-container">
+          <h1 className="page-title">{mode==="signup"? "SignUp" : "Login"}</h1>
+          <form className="auth-form">
+
+            <div className="form-group">
+              <label className="form-label" htmlFor="email">Email</label>
+              <input className="form-input" type="email" id="email"/>
+            </div>
+
+            <div className="form-group">
+              <label className="form-label" htmlFor="password">Password</label>
+              <input className="form-input" type="passwords" id="password"/>
+            </div>
+
+            <button type="submit" className="btn btn-primary btn-large">{mode==="signup"? "SignUp" : "Login"}</button>
+          </form>
+
+          <div className="auth-switch">
+            {mode==="signup" ? 
+              (<p>Already have an account? <span className="auth-link" onClick={()=>setMode("login")}>Login</span></p>) 
+              :(<p>Don't have an account? <span className="auth-link" onClick={()=>setMode("signup")}>SignUp</span></p>) 
+            }
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
