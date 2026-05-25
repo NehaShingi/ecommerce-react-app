145413ce3114ff7e0b3fe15aa265cae82b53656c
NehaShingi <shingineha836@gmail.com>
2026-05-25 19:28:25 +0530

    Add cart functionality

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,11 +1,16 @@
 import { useNavigate, useParams } from "react-router-dom"
 import { useEffect, useState } from "react";
 import { getProductById } from "../data/products";
+import { useCart } from "../context/CartContext";
 
 const ProductDetails = () => {
     const {id}=useParams();
     const [product,setProduct]=useState(null);
     const navigate=useNavigate();
+
+    const {addToCart,cartItems}=useCart();
+    
+
 
     useEffect(()=>{
         const foundProduct= getProductById(id);
@@ -22,6 +27,12 @@ const ProductDetails = () => {
         return <h1>Loading...</h1>
     }
 
+    const productInCart=cartItems.find((item)=>item.id===product.id);
+
+    const productQuantityLabel=productInCart 
+    ? `(${productInCart.quantity})`
+    : "";
+
   return (
     <div className="page">
       <div className="container">
@@ -33,7 +44,11 @@ const ProductDetails = () => {
                 <h1 className="product-detail-name">{product.name}</h1>
                 <p className="product-detail-price">${product.price}</p>
                 <p className="product-detail-description">{product.description}</p>
-                <button className="btn btn-primary">Add to Cart</button>
+                <button className="btn btn-primary" onClick={()=>{
+                    addToCart(product.id);
+                }}>
+                        Add to Cart {productQuantityLabel}
+                </button>
             </div>
         </div>
       </div>
