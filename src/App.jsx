d47aef1e8cef58e153a4406ebe68a4a130e5de86
NehaShingi <shingineha836@gmail.com>
2026-05-25 17:07:28 +0530

    Add product details page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Checkout from "./pages/Checkout"
 import Navbar from "./components/navbar"
 import "./App.css"
 import AuthProvider from "./context/AuthContext"
+import ProductDetails from "./pages/ProductDetails"
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' element={<Home/>}></Route>
           <Route path='/auth' element={<Auth/>}></Route>
           <Route path='/checkout' element={<Checkout/>}></Route>
+          <Route path='/products/:id' element={<ProductDetails/>}></Route>
         </Routes>
       </div>
     </AuthProvider>
